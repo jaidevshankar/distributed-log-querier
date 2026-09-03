61019ae7ac95392f83f2f7191575d54e97ac3cc3
Neel <nnigam2@illinois.edu>
2026-09-02 21:23:25 -0400

    updated HandleGrep()

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"net/rpc"
 	"os"
 	"bytes"
+	"os/exec"
 )
 
 type GrepArgs struct {
@@ -29,30 +30,38 @@ func (node *Node) HandleGrep (args *GrepArgs, reply *GrepReply) error {
 	// implementation here to execute the command.
 	// args.Input is just the raw input string that the user passed (i.e. everything after `grep` e.g. "-i -E "regex here")
 	// Need to exec grep on the filepath `logs/machine.${dlq.Me}.log`
-	// example: cmd := exec.Command("sh", "-c", fullCommand); output, err = cmd.CombinedOutput()
-		// don't use exec.Command("grep", ...args) directly since apparently it doesn't support all the flags and shit
 	// Need to set reply.LineCount (and maybe reply.Output) and reply.Error
-	filepath := 'logs/machine.${node.Me}.log'
+	filepath := fmt.Sprintf("logs/machine.%d.log", node.Me)
+	// extract input
 	arguments := args.Input
-	fullCommand := fmt.Sprintf("grep", arguments, filepath)
+	// create grep command
+	fullCommand := fmt.Sprintf("grep %s %s", arguments, filepath)
+	//call grep
 	cmd := exec.Command("sh", "-c", fullCommand)
-	output, err = cmd.CombinedOutput()
+	output, err := cmd.CombinedOutput()
+	// check errors
 	if err != nil {
 		if exitErr, ok := err.(*exec.ExitError); ok{
 			exitCode := exitErr.ExitCode()
 			// exit code = 1 means that there were no pattern matches which is not an actual error
 			if exitCode == 1 {
-				reply.lines = ""
-				reply.lineCount = 0
+				reply.Output = ""
+				reply.LineCount = 0
 				reply.Error = false
 				return nil
 			}
 		}
+		// if not exit code = 1, then valid error
+			// potential errors: 
+		// user tries to pass a filepath. once we append our own machine.i.log filepath, exec grep will return an error, so can return a generic error in this case. TODO: tell user not to specify a filepath in grep command
+		// grep fails due to invalid pattern/ user input, will be caught by the exec command so can return an error
 		reply.Error = true
-		fmt.Printf("Error executing command : %s/n : ", err)
+		fmt.Printf("Error executing command : %s\n ", err)
 		return nil
 	}
+	// successful grep
 	reply.Error = false
+	// use scanner to count lines and set reply.LineCount and reply.Output
 	scanner := bufio.NewScanner(bytes.NewReader(output))
 	count := 0
 	for scanner.Scan() {
@@ -60,13 +69,6 @@ func (node *Node) HandleGrep (args *GrepArgs, reply *GrepReply) error {
 	}
 	reply.LineCount = count
 	reply.Output = string(output)
-
-
-	// potential errors: 
-// user tries to pass a filepath. once we append our own machine.i.log filepath, exec grep will return an error, so can return a generic error in this case. TODO: tell user not to specify a filepath in grep command
-// grep fails due to invalid pattern/ user input, will be caught by the exec command so can return an error
-// no lines matched, which will technically return an error when calling cmd.CombinedOutput(). the exit status will be 1, in which case we don't want to set reply.Error to true. we just want to set reply.Output = "" and reply.LineCount = 0
-
 // return nil since there were no errors
 	return nil
 }
